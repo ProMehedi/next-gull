3ae4e9b7fc93d19595a99e88dc5b288b121541fc
promehedi <mmhasan563@gmail.com>
2021-08-08 19:09:24 +0600

    Stats Data

---
+++ data/stats.js
@@ -0,0 +1,30 @@
+const stats = [
+  {
+    id: 1,
+    title: 'New Leds',
+    desc: '205',
+    icon: <img src='icons/user.svg' />,
+    variant: 'primary',
+  },
+  {
+    id: 2,
+    title: 'Sales',
+    desc: '$1,000,00',
+    icon: <img src='icons/sale.svg' />,
+    variant: 'secondary',
+  },
+  {
+    id: 3,
+    title: 'Orders',
+    desc: '1,000',
+    icon: <img src='icons/orders.svg' />,
+    variant: 'success',
+  },
+  {
+    id: 4,
+    title: 'Expense',
+    desc: '$1,200',
+    icon: <img src='icons/expense.svg' />,
+    variant: 'warning',
+  },
+]
